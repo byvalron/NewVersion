486a0beea83067b6c17721d33980fd6cd5d0c99b
Vitalii <byvalron@gmail.com>
2017-02-10 18:27:47 +0200

    Stick Jsch

---
+++ app/src/main/java/com/example/newversion/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.newversion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private View thumb, joystick;
+    private TextView centerText, textView, pow;
+
+
+
+    public String MoveForward = "echo -e -n \"\\x31\\x2c\\x6f\\x6e\\x2c\\x%1$s\\x2c\\x%2$s\\x2c\" > /dev/ttyUSB0";
+    public String MoveLeft = "echo -e -n \"\\x32\\x2c\\x6f\\x6e\\x2c\\x%1$s\\x2c\\x%2$s\\x2c\" > /dev/ttyUSB0";
+    public String MoveStop = "echo -e -n \"\\x35\\x2c\\x6f\\x6e\\x2c\" > /dev/ttyUSB0";
+    public String MoveRight = "echo -e -n \"\\x33\\x2c\\x6f\\x6e\\x2c\\x%1$s\\x2c\\x%2$s\\x2c\" > /dev/ttyUSB0";
+    public String MoveBack = "echo -e -n \"\\x34\\x2c\\x6f\\x6e\\x2c\\x%1$s\\x2c\\x%2$s\\x2c\" > /dev/ttyUSB0";
+
+    private static final int SCALE = 0xFF;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        thumb = findViewById(R.id.thumb);
+        joystick = findViewById(R.id.joystick);
+        centerText = (TextView) findViewById(R.id.center_text);
+        textView = (TextView) findViewById(R.id.textView);
+        pow = (TextView) findViewById(R.id.pow);
+
+        joystick.setOnTouchListener(new View.OnTouchListener() {
+            float jRadius = getResources().getDimension(R.dimen.joystick_radius) /2;
+            float tRadius = getResources().getDimension(R.dimen.thumb_radius) / 2;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        moveThumb(0, 0);
+                    case MotionEvent.ACTION_MOVE:
+                        moveThumb(event.getX() - jRadius, event.getY() - jRadius);
+                        double angle = getAngle(event.getX() - jRadius, event.getY() - jRadius);
+                        double POWER1 = getPower(event.getX() - jRadius, event.getY() - jRadius);
+                        final String POWER2 = Integer.toHexString(Integer.parseInt(String.valueOf((int)POWER1))).toUpperCase();
+
+                        final String UP = String.format(MoveForward, POWER2, POWER2);
+                        final String LEFT = String.format(MoveLeft, POWER2, POWER2);
+                        final String RIGHT = String.format(MoveRight, POWER2, POWER2);
+                        final String DOWN = String.format(MoveBack, POWER2, POWER2);
+
+                        final double RADIAN = 180 / Math.PI;
+                        final double NEXT = angle * RADIAN;
+                        if(NEXT >= 338.5 || NEXT < 22.5 ) {
+                            SshWrapper.getInstance().runCommand(RIGHT);
+                            System.out.println(RIGHT);
+                            textView.setText("Direction: RIGHT");
+                        } else if(NEXT >= 22.5 && NEXT < 67.5 ) {
+                            textView.setText("Direction: DOWN_RIGHT");
+                        } else if(NEXT >= 67.5 && NEXT < 113.5 ) {
+                            SshWrapper.getInstance().runCommand(DOWN);
+                            System.out.println(DOWN);
+                            textView.setText("Direction: DOWN");
+                        } else if(NEXT >= 113.5 && NEXT < 158.5 ) {
+                            textView.setText("Direction: DOWN_LEFT");
+                        } else if(NEXT >= 158.5 && NEXT < 203.5 ) {
+                            SshWrapper.getInstance().runCommand(LEFT);
+                            System.out.println(LEFT);
+                            textView.setText("Direction: LEFT");
+                        } else if(NEXT >= 203.5 && NEXT < 248.5 ) {
+                            textView.setText("Direction: UP_LEFT");
+                        } else if(NEXT >= 248.5 && NEXT < 293.5 ) {
+                            SshWrapper.getInstance().runCommand(UP);
+                            System.out.println(UP);
+                            textView.setText("Direction: UP");
+                        } else if(NEXT >= 293.5 && NEXT < 338.5 ) {
+                            textView.setText("Direction: UP_RIGHT");
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        moveThumb(0, 0);
+                        SshWrapper.getInstance().runCommand(MoveStop);
+                        System.out.println(MoveStop);
+                        textView.setText("Direction: ");
+                        break;
+                    default:
+                }
+                return true;
+            }
+            //Перемещение стика
+            public void moveThumb(float x, float y) {
+                double angle = getAngle(x, -y);
+                double radius = getRadius(x, -y);
+                double POWER = getPower(x, -y);
+                final double RADIAN = 180 / Math.PI;
+                final double NEXT = angle * RADIAN;
+
+                centerText.setText(String.valueOf((int) (NEXT)) + ":"+ Integer.toHexString(Integer.parseInt(String.valueOf((int) (POWER)))).toUpperCase());
+                pow.setText(String.valueOf((int) getPower(x) + " : " + (int) getPower(-y)));
+                x = (float) (radius * Math.cos(angle)) * jRadius + joystick.getX() + jRadius - tRadius;
+                y = -(float) (radius * Math.sin(angle)) * jRadius + joystick.getY() + jRadius - tRadius;
+
+                thumb.setX(x);
+                thumb.setY(y);
+            }
+            //Угол
+            public  double getAngle(float x, float y) {
+                double theta;
+                x /= jRadius;
+                y /= jRadius;
+                if (x > 0) {
+                    theta = Math.atan(y / x);
+                    if (y < 0) {
+                        theta += 2 * Math.PI;
+                    }
+                } else if (x < 0) {
+                    theta = Math.atan(y / x) + Math.PI;
+                } else {
+                    if (y > 0) {
+                        theta = Math.PI / 2;
+                    } else if (y < 0) {
+                        theta = -Math.PI / 2;
+                    } else {
+                        theta = 0;
+                    }
+                }
+                return theta;
+            }
+            //Радиус
+            public double getRadius(float x, float y) {
+                x /= jRadius;
+                y /= jRadius;
+                return Math.min(Math.sqrt(x * x + y * y), (jRadius - tRadius) / jRadius);
+            }
+            // Скорость
+            public double getPower(float x, float y) {
+                x /= jRadius;
+                y /= jRadius;
+                double radius = (jRadius - tRadius) / jRadius;
+                return Math.min(Math.sqrt(x * x + y * y), radius) / radius * SCALE;
+            }
+
+            // Скоростьпо X Y
+            public double getPower(float x) {
+                x /= jRadius - tRadius;
+                return Math.min(Math.rint(Math.abs(x) * SCALE), SCALE) * Math.signum(x);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/newversion/SshWrapper.java
@@ -0,0 +1,226 @@
+package com.example.newversion;
+
+import android.content.Context;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class SshWrapper {
+
+    String username = "root";
+    String password = "root";
+    String hostname = "192.168.1.2";
+    int port = 22;
+
+    public final String TAG = "Test";
+    private boolean isFirstConnect = false;
+    private boolean isConnecting = false;
+
+    private static SshWrapper   _instance;
+    private SshWrapper() { }
+
+    public synchronized static SshWrapper getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new SshWrapper();
+        }
+        return _instance;
+    }
+
+    private JSch sshChannel = null;
+    public Session session = null;
+
+    private Session getSession(/*Context context*/) throws Exception {
+        try {
+            ChannelExec testChannel = (ChannelExec) session.openChannel("exec");
+            testChannel.setCommand("true");
+            testChannel.connect();
+           // LogWrapper.getInstance().addToListLog("Tested session successfully, use it again");
+            testChannel.disconnect();
+        }
+        catch (Throwable t) {
+
+            sshChannel = new JSch();
+
+            session = sshChannel.getSession(username, hostname, port);
+
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+
+          //  LogWrapper.getInstance().addToListLog("Try connect...");
+            session.connect();
+
+          //  String err = context.getResources().getString(R.string.errConnectionSuccessful);
+           // LogWrapper.getInstance().addToListLog(err);
+
+        }
+        return session;
+    }
+
+    public String firstConnect(Context context){
+        if(isFirstConnect){
+            return "";
+        }
+        isFirstConnect = true;
+
+        return connect(context);
+    }
+
+    public String connect(Context context){
+
+        String err = "";
+        if(isConnecting) {
+           // err = context.getResources().getString(R.string.error_connecting_in_process);
+          //  LogWrapper.getInstance().addToListLog(err);
+            return err;
+        }
+        isConnecting = true;
+
+        if (sshChannel != null)
+        {
+            disConnect();
+        }
+
+        if (hostname != null
+                && !hostname.isEmpty()
+                && !hostname.equals("null"))
+        {
+            try {
+                session = getSession(/*context*/);
+            }
+            catch (JSchException e) {
+               // err = e.getMessage();
+               // LogWrapper.getInstance().addToListLog(err);
+                return err;
+            }
+            catch (Exception e) {
+              //  err = e.getMessage();
+               // LogWrapper.getInstance().addToListLog(err);
+                return err;
+            }
+            finally {
+                isConnecting = false;
+            }
+        }
+        else
+        {
+           // err = context.getResources().getString(R.string.errUnknownIP);
+           // LogWrapper.getInstance().addToListLog(err);
+        }
+
+        isConnecting = false;
+        return err;
+    }
+
+    public void disConnect(){
+
+        if (sshChannel != null && session!= null && session.isConnected())
+        {
+           // LogWrapper.getInstance().addToListLog("Set disConnect");
+            session.disconnect();
+        }
+
+        session = null;
+        sshChannel = null;
+
+    }
+
+    public String runCommand(String command){
+
+        String err = "";
+/*        if (!InternetConnection.isOnline(mContext)) {
+            disConnect();
+           // err = mContext.getString(R.string.error_no_connection_internet);
+            return err;
+        }*/
+
+        if(isConnecting) {
+           // err = mContext.getResources().getString(R.string.error_connecting_in_process);
+           // LogWrapper.getInstance().addToListLog(err);
+            return err;
+        }
+
+        if(session == null || !session.isConnected()){
+           // err = mContext.getResources().getString(R.string.errNoConnect);
+
+            return err;
+        }
+
+        Channel channel= null;
+        try {
+            channel = session.openChannel("exec");
+        }
+        catch (JSchException e) {
+
+          //  err = e.getMessage();
+           // LogWrapper.getInstance().addToListLog(err);
+            return err;
+        }
+
+        ((ChannelExec) channel).setCommand(command);
+        channel.setInputStream(null);
+        ((ChannelExec)channel).setErrStream(System.err);
+
+        try {
+
+            {
+                channel.connect();
+            }
+
+            final InputStream in = channel.getInputStream();
+            byte[] tmp=new byte[1024];
+            while(true){
+                while(in.available()>0){
+                    int i = in.read(tmp, 0, 1024);
+                    if(i<0)break;
+                    System.out.print(new String(tmp, 0, i));
+                    if(i != 0) {
+                      //  LogWrapper.getInstance().addToListLog(new String(tmp, 0, i));
+                    }
+                }
+                if(channel.isClosed()){
+                    if(in.available()>0) continue;
+                    int status = channel.getExitStatus();
+                    System.out.println("exit-status: " + status);
+                    if(status != 0) {
+                       // LogWrapper.getInstance().addToListLog("exit-status: " + status);
+                    }
+                    break;
+                }
+                try{
+                    Thread.sleep(1000);
+                }
+                catch(Exception e){
+                   // LogWrapper.getInstance().addToListLog(e.getMessage());
+                }
+            }
+        }
+        catch (JSchException e) {
+
+           // err = e.getMessage();
+           // LogWrapper.getInstance().addToListLog(err);
+            return err;
+        }
+        catch (IOException e) {
+
+            //err = e.getMessage();
+          //  LogWrapper.getInstance().addToListLog(err);
+            return err;
+        }
+
+        channel.disconnect();
+        return err;
+    }
+
+    public void Destroy(){
+        disConnect();
+    }
+}
