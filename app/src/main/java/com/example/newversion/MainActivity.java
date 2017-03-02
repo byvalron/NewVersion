d4168abb8cbfbbb90ca62f5982e9ecd9cc989c3c
Vitalii <byvalron@gmail.com>
2017-03-02 09:55:13 +0200

    01.03

--- app/src/main/java/com/example/newversion/MainActivity.java
+++ app/src/main/java/com/example/newversion/MainActivity.java
@@ -7,12 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private View thumb, joystick;
-    private ImageButton stop;
+    private ImageButton stop, uu;
+
+    boolean flag = true;
 
     public String MoveForward = "echo -e -n \"\\x31\\x2c\\x6f\\x6e\\x2c\\x%1$s\\x2c\\x%2$s\\x2c\" > /dev/ttyUSB0";
     public String MoveLeft = "echo -e -n \"\\x32\\x2c\\x6f\\x6e\\x2c\\x%1$s\\x2c\\x%2$s\\x2c\" > /dev/ttyUSB0";
@@ -32,14 +35,10 @@ public class MainActivity extends AppCompatActivity {
         stop = (ImageButton) findViewById(R.id.stop);
         thumb = findViewById(R.id.thumb);
         joystick = findViewById(R.id.joystick);
-
+        uu = (ImageButton) findViewById(R.id.uu);
 
         context = this;
         MainActivity.context = getApplicationContext();
-       //SshWrapper.getInstance().connect(MainActivity.context);
-        new ConnectTask().execute();
-      //  new Command().execute();
-
 
         joystick.setOnTouchListener(new View.OnTouchListener() {
             float jRadius = getResources().getDimension(R.dimen.joystick_radius) /2;
@@ -47,16 +46,6 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                stop.setOnClickListener(new View.OnClickListener()
-                {
-                    public void onClick(View v)
-                    {
-                        moveThumb(0, 0);
-                        SshWrapper.getInstance().runCommand(MoveStop);
-                        System.out.println(MoveStop);
-                    }
-                });
-
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         moveThumb(0, 0);
@@ -75,22 +64,22 @@ public class MainActivity extends AppCompatActivity {
                         final double RADIAN = 180 / Math.PI;
                         final double NEXT = angle * RADIAN;
                         if(NEXT >= 338.5 || NEXT < 22.5 ) {
-                            SshWrapper.getInstance().runCommand(RIGHT);
+                            command = RIGHT;
                             System.out.println(RIGHT);
                         } else if(NEXT >= 22.5 && NEXT < 67.5 ) {
                             System.out.println("DOWN_RIGHT");
                         } else if(NEXT >= 67.5 && NEXT < 113.5 ) {
-                            SshWrapper.getInstance().runCommand(DOWN);
+                            command = DOWN;
                             System.out.println(DOWN);
                         } else if(NEXT >= 113.5 && NEXT < 158.5 ) {
                             System.out.println("DOWN_LEFT");
                         } else if(NEXT >= 158.5 && NEXT < 203.5 ) {
-                            SshWrapper.getInstance().runCommand(LEFT);
+                            command = LEFT;
                             System.out.println(LEFT);
                         } else if(NEXT >= 203.5 && NEXT < 248.5 ) {
                             System.out.println("UP_LEFT");
                         } else if(NEXT >= 248.5 && NEXT < 293.5 ) {
-                            SshWrapper.getInstance().runCommand(UP);
+                            command = UP;
                             System.out.println(UP);
                         } else if(NEXT >= 293.5 && NEXT < 338.5 ) {
                             System.out.println("UP_RIGHT");
@@ -98,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case MotionEvent.ACTION_UP:
                         moveThumb(0,0);
+                        command = MoveStop;
+                        System.out.println(MoveStop);
                     //    moveThumb(event.getX() - jRadius, event.getY() - jRadius);
                     //    TranslateAnimation a = new TranslateAnimation(
                      //           Animation.ABSOLUTE, 0,
@@ -114,6 +105,16 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     default:
                 }
+                stop.setOnClickListener(new View.OnClickListener()
+                {
+                    public void onClick(View v)
+                    {
+                        moveThumb(0, 0);
+                        command = MoveStop;
+                        System.out.println(MoveStop);
+                    }
+                });
+                new CommandTask().execute();
                 return true;
             }
             //Перемещение стика
@@ -165,31 +166,38 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        uu.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                if(flag) {
+                    new ConnectTask().execute();
+                    uu.setBackgroundResource(R.drawable.uuu);
+                    Toast.makeText(MainActivity.getAppContext(),"Connect", Toast.LENGTH_SHORT).show();
+                    System.out.println("Connect");
+                }else{
+                    flag = !flag;
+                    uu.setBackgroundResource(R.drawable.u);
+                    Toast.makeText(MainActivity.getAppContext(),"FALSE!!!", Toast.LENGTH_SHORT).show();
+                    System.out.println("FALSE!!!");
+                }
+            }
+        });
     }
-    class ConnectTask extends AsyncTask<Void, Void, String> {
-
+    class ConnectTask extends AsyncTask<Void, Void, Void> {
         @Override
-        protected String doInBackground(Void... params) {
+        protected Void doInBackground(Void... params) {
             String result = SshWrapper.getInstance().firstConnect(context);
-           // String result1 = SshWrapper.getInstance().runCommand(command);
             return null;
-        }
-        @Override
-        protected void onPostExecute(String result) {
-            String result1 = SshWrapper.getInstance().runCommand(command);
-            super.onPostExecute(result1);
-
         }
     }
-/*    class Command extends AsyncTask<String, String, String> {
-
+    class CommandTask extends AsyncTask<Void, Void, Void> {
         @Override
-        protected String doInBackground(String... strings) {
-
+        protected Void doInBackground(Void... params) {
+            String result1 = SshWrapper.getInstance().runCommand(command);
             return null;
         }
-    }*/
-
+    }
 
     public static Context getAppContext() {
         return MainActivity.context;
